8b4c8afbae0738b7e5d94c9d2a95d9a91f6bc82b
Bertha Rasoloaritiana <rasoloaritianaherinomena@gmail.com>
2026-02-09 22:28:59 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Petit script pour faire apparaître les éléments au défilement
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+// Lancer une fois au chargement
+reveal();
